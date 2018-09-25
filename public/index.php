276f618cbcbe0c531a1be76599076948e4d1b2e5
e <2455359340@qq.com>
2018-09-25 16:56:57 +0800

    '初始化项目'

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php
+
+define(ROOT,__DIR__.'/../');
+
+//调用自己自动加载
+function load($class)
+{
+    $path = str_replace('\\','/',$class);
+    require(ROOT.$path.'.php');
+}
+spl_autoload_register('load');
+
+
+//解析路由
+$controller = '\controllers\IndexController';
+$action = 'index';
+if(isset($_SERVER['PATH_INFO']))
+{
+    $router = explode('/',$_SERVER['PATH_INFO']);
+    $controller = '\controller\\'.ucfirst($router[1]).'Controller';
+    $action = $router[2];
+}
+$c = new $controller;
+$c->$action();
